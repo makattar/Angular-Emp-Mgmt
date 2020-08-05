ffaf2da1f724312edc8869a62a6624a159435d14
unknown <mahabkadar25@gmail.com>
2020-08-05 13:50:56 +0530

    http to https Node Pagination

--- src/app/emplist.service.ts
+++ src/app/emplist.service.ts
@@ -10,10 +10,10 @@ export class EmplistService {
   //private _empListUrl="http://localhost:3000/api/emp-list";
   //private _empListUrl="http://localhost:3000/employees/emp-list";
   //private _empListUrl="https://mydbpg.herokuapp.com/employees/emp-list";
-  //private _empListUrl=`http://mydbpg.herokuapp.com/employees/emp-list?page=${this.page}`
+  //private _empListUrl=`https://mydbpg.herokuapp.com/employees/emp-list?page=${this.page}`
   constructor(private http:HttpClient) { }
   getEmpList(page){
-    this._empListUrl=`http://mydbpg.herokuapp.com/employees/emp-list?page=${page}`
+    this._empListUrl=`https://mydbpg.herokuapp.com/employees/emp-list?page=${page}`
     return this.http.get<any>(this._empListUrl);
   }
 }
