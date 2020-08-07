f3e5f4b2582b060dd05abd852d3be25fe3f12f77
unknown <mahabkadar25@gmail.com>
2020-08-07 12:25:10 +0530

    Added encryption

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {EnrollmentService} from '../enrollment.service';
 import {Router} from '@angular/router';
+import * as CryptoJS from 'crypto-js';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -9,13 +10,21 @@ import {Router} from '@angular/router';
 export class SignInComponent implements OnInit {
  loginUserModel={email:'',password:''};
  public error = "";
+ encPassword:string="makattar"
+ conversionEncryptedPassword:string;
+ conversionDecryptedPassword:string;
   constructor(private _loginService:EnrollmentService,private _router:Router) { }
 
   ngOnInit(): void {
   }
   loginUser(){
    //console.log(this.loginUserModel)
-   this._loginService.loginUser(this.loginUserModel).subscribe(
+  this.conversionEncryptedPassword=CryptoJS.AES.encrypt(this.loginUserModel.password.trim(),this.encPassword.trim()).toString();
+  console.log("Encrypted password is :",this.conversionEncryptedPassword); 
+  this.conversionDecryptedPassword=CryptoJS.AES.decrypt(this.conversionEncryptedPassword.trim(),this.encPassword.trim()).toString(CryptoJS.enc.Utf8);
+  console.log("Decrypted password is :",this.conversionDecryptedPassword);
+  this.loginUserModel.password=this.conversionEncryptedPassword;
+  this._loginService.loginUser(this.loginUserModel).subscribe(
      res=>{
        console.log(res)
        if(res.status==="Failed"){
